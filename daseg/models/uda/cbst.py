50caca86b5a10688460d3da6e425b1c3c2a9c983
谢昕辰 <xinchen.xie@qq.com>
2021-12-21 21:38:04 +0800

    merge

--- daseg/models/uda/cbst.py
+++ daseg/models/uda/cbst.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-from daseg.models.builder import UDA
-from .base_uda import UDADecorator
-=======
 import math
 import os
 import random
@@ -17,19 +13,14 @@ from mmseg.core import add_prefix
 from mmseg.models import build_segmentor
 from .uda_decorator import UDADecorator, get_module
 from daseg.models.builder import UDA
->>>>>>> 4be082c3119107bf2d71e1accfeb53757c5f0560
 
 
 @UDA.register_module()
 class CBST(UDADecorator):
     def __init__(self, **cfg):
-<<<<<<< HEAD
-        super().__init__(**cfg)
-=======
         super(CBST, self).__init__(**cfg)
         self.max_iters = cfg['max_iters']
         self.curr_iter = 0
->>>>>>> 4be082c3119107bf2d71e1accfeb53757c5f0560
 
     def train_step(self, data_batch, optimizer, **kwargs):
         optimizer.zero_grad()
@@ -40,11 +31,6 @@ class CBST(UDADecorator):
                        num_samples=len(data_batch['img_metas']))
         return outputs
 
-<<<<<<< HEAD
-    def forward_train(self, img, img_metas, gt_semantic_seg, target_img,
-                      target_img_metas, target_gt_semantic_seg):
-        pass
-=======
     def get_weights_k(self, target_logits: torch.Tensor):
         return torch.ones(target_logits.shape)
 
@@ -62,4 +48,3 @@ class CBST(UDADecorator):
         # train with source img
 
         return log_vars
->>>>>>> 4be082c3119107bf2d71e1accfeb53757c5f0560
